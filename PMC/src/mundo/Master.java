1d28415f1c6260c3675422aa78074c5c3d92a921
olpino521 <ol.pino521@uniandes.edu.co>
2015-04-26 22:20:15 -0500

    algunas queries

--- PMC/src/mundo/Master.java
+++ PMC/src/mundo/Master.java
@@ -3,6 +3,9 @@ package mundo;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.Date;
 
 public class Master {
 
@@ -57,7 +60,16 @@ private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 		PreparedStatement prep = null;
 		String q ="SELECT * FROM temperatura";
 		prep = conexion.prepareStatement(q);
-		prep.executeQuery();
+		ResultSet rs = prep.executeQuery();
+		Quark quark = new  Quark(null, 0, 0, 0);
+		while(rs.next())
+		{
+			long temp = rs.getLong("TEMPERATURA");
+			Timestamp t = rs.getTimestamp("TIMESTAMP");
+			int tiempo = t.getDate();
+			quark.setFecha(new Date(tiempo));
+			quark.setTemperatura(temp);
+		}
 		
 	}
 }
